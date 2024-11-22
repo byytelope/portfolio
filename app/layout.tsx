a4011c8b7f75c326d537a9d529408d5e3d1d9165
Mohamed Shadhaan <shadhanm@gmail.com>
2024-11-23 07:53:40 +0800

    fix analytics?

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Inconsolata } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
